4bfdadecc82b50958e9a949749906654b047fbd3
aFraley <humanimalmammal@webtv.net>
2017-02-07 10:16:46 -0600

    fixed url issues

--- anywhere_app/blog/urls.py
+++ anywhere_app/blog/urls.py
@@ -4,9 +4,8 @@ from django.contrib.auth.decorators import login_required
 from . import views
 
 urlpatterns = [
-    url(r'api/', include('blog.api.urls', namespace='api')),
     url(r'create/$', login_required(views.TopicCreate.as_view()), name='create'),
     url(r'topics/(?P<topic_id>\d+)/$', login_required(views.TopicDetail.as_view()), name='topic'),
-    url(r'comments/(?P<topic_id>\d+)/$', login_required(views.Comments.as_view()), name='topic'),
+    url(r'comments/(?P<topic_id>\d+)/$', login_required(views.Comments.as_view()), name='comments'),
     url(r'$', login_required(views.TopicsView.as_view()), name='topics')
 ]
